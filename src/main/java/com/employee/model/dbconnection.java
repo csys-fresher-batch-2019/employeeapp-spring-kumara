8e9bcc1217c79b24a06f42de819c1f4f6b9b6150
kumarakkani <kumarakkani888@gmail.com>
2020-03-03 09:49:29 +0530

    addd

--- src/main/java/com/employee/model/dbconnection.java
+++ src/main/java/com/employee/model/dbconnection.java
@@ -14,8 +14,8 @@ public class dbconnection {
 		//TimeZone.setDefault(timeZone);
 		Class.forName("oracle.jdbc.driver.OracleDriver");
 		
-		//Connection con = DriverManager.getConnection("jdbc:oracle:thin:@13.235.147.120:1521:XE", "kumarakkani", "kumarakkani");
-		Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "system", "oracle");
+		Connection con = DriverManager.getConnection("jdbc:oracle:thin:@13.235.147.120:1521:XE", "kumarakkani", "kumarakkani");
+		//Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "system", "oracle");
 		// Statement stmt=con.createStatement();
 		return con;
 
